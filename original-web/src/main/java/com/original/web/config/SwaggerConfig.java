37554953e2774e58a6d9ff3ee2898a3971d4f8c5
卢杭启 <itoutsource.cz45@trinasolar.com>
2018-09-25 14:10:06 +0800

    swagger

---
+++ original-web/src/main/java/com/original/web/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package com.original.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * SwaggerConfig
+ *
+ * @author luhangqi
+ * @date 2018/9/25
+ */
+@Configuration
+@EnableSwagger2
+@EnableWebMvc
+@ComponentScan(basePackages = "com.original.web.controller")
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("XXX项目接口文档")
+                .description("XXX项目接口测试")
+                .version("1.0.0")
+                .termsOfServiceUrl("")
+                .license("")
+                .licenseUrl("")
+                .build();
+    }
+}
